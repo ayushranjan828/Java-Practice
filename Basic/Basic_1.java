1aa085fa7874e7586e2f074bfcfb6a9a747f02a4
Ayush Ranjan <119885031+ayushranjan828@users.noreply.github.com>
2024-04-02 22:20:22 +0530

    Add files via upload

---
+++ Basic/Basic_1.java
@@ -0,0 +1,12 @@
+public class Basic_1 
+{
+    static void show()
+    {
+        System.out.println("Show World");
+    }
+    public static void main(String[] args)
+    {
+        show();
+        System.out.println("Hello World");
+    }    
+}
---
+++ Basic/Comment.java
@@ -0,0 +1,12 @@
+public class Comment
+// Single Line Comment
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }    
+}
+
+/*
+    Multi-Line Comment
+ */
