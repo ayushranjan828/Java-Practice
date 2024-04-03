8f8a29d19cb80b8f3235f48bf3a48ab6164a35a1
Ayush Ranjan <119885031+ayushranjan828@users.noreply.github.com>
2024-04-03 22:13:37 +0530

    Create Sum.java

---
+++ Variable/Sum.java
@@ -0,0 +1,9 @@
+public class Sum
+{
+    public static void main(String[] args)
+    {
+        int a = 6, b = 7, sum;
+        sum = a + b;
+        System.out.println("Sum of "+a+" + "+b+" = "+sum);
+    }
+}
