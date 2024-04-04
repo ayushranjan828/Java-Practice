a3a112f42a78a37f94c8153fa6732f1b2c9116e5
Ayush Ranjan <119885031+ayushranjan828@users.noreply.github.com>
2024-04-04 22:34:57 +0530

    Add files via upload

---
+++ Casting/Explicit.java
@@ -0,0 +1,11 @@
+public class Explicit 
+{
+    public static void main(String[] args)
+    {
+        double a = 10.5;
+        int b = (int)a;
+
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+    }    
+}
