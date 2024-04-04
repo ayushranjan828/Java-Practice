d87523e3a4bc808c6a77f2dddef14dedd91a7019
Ayush Ranjan <119885031+ayushranjan828@users.noreply.github.com>
2024-04-04 22:34:25 +0530

    Create Implicit.java

---
+++ Casting/Implicit.java
@@ -0,0 +1,11 @@
+public class Implicit
+{
+    public static void main(String[] args)
+    {
+        int a = 18;
+        double b = a;
+
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+    }
+}
