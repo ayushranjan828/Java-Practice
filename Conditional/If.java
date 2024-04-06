7f571066d1316d556dad82672e98753313212694
Ayush Ranjan <119885031+ayushranjan828@users.noreply.github.com>
2024-04-06 17:10:01 +0530

    Create If.java

---
+++ Conditional/If.java
@@ -0,0 +1,11 @@
+class If 
+{
+    public static void main(String[] args) 
+    {
+        double a = 0.5;
+        if (a>0)
+        {
+            System.out.println(a + " is a Positive Number !"); 
+        }
+    }
+}
