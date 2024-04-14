2702f9f53a7343b88b26693c470c946cf7afc8e1
Ayush Ranjan <119885031+ayushranjan828@users.noreply.github.com>
2024-04-14 10:25:53 +0530

    Add files via upload

---
+++ Loop/For_each.java
@@ -0,0 +1,11 @@
+class For_each  
+{
+    public static void main(String[] args)
+    {
+        int arr[] = { 10, 50, 60, 80, 90 }; 
+        for (int element : arr) 
+        {
+            System.out.print(element + " ");
+        }
+    } 
+}
