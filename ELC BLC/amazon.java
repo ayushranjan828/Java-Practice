d81d05374e7cc842bb228f6c4f22ca1687af510c
Ayush Ranjan <119885031+ayushranjan828@users.noreply.github.com>
2024-04-19 10:18:07 +0530

    Add files via upload

---
+++ ELC BLC/amazon.java
@@ -0,0 +1,17 @@
+class amazon
+{
+    int a, b, c, d;
+    void input()
+    {
+        a = 10;
+        b = 20;
+    }
+    void show()
+    {
+        c = a + b;
+        d = a - b;
+
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
