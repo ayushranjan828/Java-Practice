5608b91a8c3350d570b11387370a88928cf707ee
Ayush Ranjan <119885031+ayushranjan828@users.noreply.github.com>
2024-04-20 17:44:18 +0530

    Create Main.java

---
+++ OOP/Main.java
@@ -0,0 +1,17 @@
+class Demo
+{
+    int a = 10; String s = "Ayush";
+    void Show()
+    {
+        System.out.println(a+ " "+s);
+    }
+}
+
+class Main
+{
+    public static void main(String[] args) 
+    {
+        Demo obj = new Demo();
+        obj.Show();    
+    }
+}
