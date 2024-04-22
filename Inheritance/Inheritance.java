53c786a9a0315a502dd8a049b9d9eae65d78832f
Ayush Ranjan <119885031+ayushranjan828@users.noreply.github.com>
2024-04-22 23:03:54 +0530

    Create Inheritance.java

---
+++ Inheritance/Inheritance.java
@@ -0,0 +1,9 @@
+/*
+    When we construct a new class from existing class in such a way that the new class access all the features and properties of existing class called Inheritance.
+
+    In java extends keyword is used to perform inheritance.
+    It provides code reusability.
+    In can't access private members of class through inheritance.
+    A sub class contains all the features of super class so, we should create the object of sub class.
+    Method overriding only possible through inheritance.
+*/
