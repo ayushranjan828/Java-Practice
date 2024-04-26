24ec433cebd72c7beffe32782225ebd06b383514
Ayush Ranjan <119885031+ayushranjan828@users.noreply.github.com>
2024-04-26 22:59:25 +0530

    Create abstract_class.java

---
+++ Abstract Class/abstract_class.java
@@ -0,0 +1,15 @@
+abstract class animal
+{
+
+}
+class Dog extends animal
+{
+
+}
+class abstract_class 
+{
+    public static void main(String[] args) 
+    {
+        animal obj = new Dog();
+    }
+}
