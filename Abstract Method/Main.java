4b627e2ae2fc6429574b4c69ae7cbdd4c9ad1879
Ayush Ranjan <119885031+ayushranjan828@users.noreply.github.com>
2024-04-28 23:54:01 +0530

    Create Main.java

---
+++ Abstract Method/Main.java
@@ -0,0 +1,30 @@
+abstract class Programming
+{
+    public abstract void Developer();
+}
+class HTML extends Programming
+{
+    public void Developer()
+    {
+        System.out.println("Tim Berners Lee");
+    }
+}
+class Java extends Programming
+{
+    public void Developer()
+    {
+        System.out.println("James Gosling");
+    }
+}
+
+class Main
+{
+    public static void main(String[] args) 
+    {
+        HTML obj_html = new HTML();    
+        obj_html.Developer();
+
+        Java obj_Java = new Java();
+        obj_Java.Developer();
+    }
+}
