4af0c85c229fed1b1a850fd9fe6bb28a7d2a7a74
Ayush Ranjan <119885031+ayushranjan828@users.noreply.github.com>
2024-04-30 23:09:23 +0530

    Create StaticBlock.java

---
+++ Block/StaticBlock.java
@@ -0,0 +1,14 @@
+/*
+    Static block is such kind .f block in java which get executed at the time of loading the .class file into JVM memory
+*/
+public class StaticBlock 
+{
+    static // Ststic block
+    {
+        System.out.println("Ayush Piyush");
+    }
+    public static void main(String[] args) 
+    {
+        
+    }       
+}
