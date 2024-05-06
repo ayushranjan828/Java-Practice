dd9eb1bbfd5b2fce17d7f52854df2604457b8cad
Ayush Ranjan <119885031+ayushranjan828@users.noreply.github.com>
2024-05-06 22:26:32 +0530

    Add files via upload

---
+++ New Keyword/NewKeyword_1.java
@@ -0,0 +1,12 @@
+public class NewKeyword_1 
+{
+    int a = 10;
+    NewKeyword_1()
+    {
+        System.out.println(a);   
+    }
+    public static void main(String[] args) 
+    {
+        NewKeyword_1 obj = new NewKeyword_1(); 
+    }    
+}
