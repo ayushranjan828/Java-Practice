cbb2724b9d82cf9f720207280875da46bb594710
Ayush Ranjan <119885031+ayushranjan828@users.noreply.github.com>
2024-05-06 22:25:51 +0530

    Create NewKeyword.java

---
+++ New Keyword/NewKeyword.java
@@ -0,0 +1,9 @@
+class NewKeyword
+{
+    int a = 10;
+    public static void main(String[] args) 
+    {
+        NewKeyword obj = new NewKeyword();
+        System.out.println(obj.a);    
+    }
+}
